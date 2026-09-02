4a3035f85564289fdd8430f248686aface5e0b57
Ridho <muhamadridho2404@gmail.com>
2026-09-02 19:37:30 +0700

    Add template.dir

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -134,6 +134,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates']
+    },
+]
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
