52614af375d4351e80f012ffd50b47c3a3b90626
Ridho <muhamadridho2404@gmail.com>
2026-09-12 19:35:49 +0700

    Membuat model Education

--- main/models.py
+++ main/models.py
@@ -24,3 +24,25 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Education(models.Model):
+    EDUCATION_CHOICES = [
+        ('elementary school', 'Elementary School'),
+        ('junior high school', 'Junior High School'),
+        ('high school', 'High School'),
+        ('university', 'University'), 
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    institution_name = models.CharField(max_length=255)
+    category = models.CharField(max_length=50, choices=EDUCATION_CHOICES, default='')
+    thumbnail = models.URLField(blank=True, default='')
+    year_started = models.IntegerField()
+    year_ended = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.institution_name
+
+    @property
+    def is_ongoing(self):
+        return self.year_ended is None
