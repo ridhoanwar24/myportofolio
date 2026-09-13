fa0360a4ab2790d6551e05513bb668a050c3202d
Ridho <muhamadridho2404@gmail.com>
2026-09-13 22:09:26 +0700

    Konfigurasi Routing URL

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Education
 
 
 def show_main(request):
@@ -23,3 +23,12 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+
+def show_education(request):
+    context = {
+        "full_name": "Muhammad Ridho Anwar",
+        "nickname": "Ridho",
+        "education_list": Education.objects.all(),
+    }
+    return render(request, "education.html", context)
